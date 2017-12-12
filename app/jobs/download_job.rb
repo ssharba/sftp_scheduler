e0bd1c1d70f386fdce3c5300df2c34a55dc32d73
louy755 <lumberjack1694@yahoo.com>
2017-12-12 16:57:39 -0600

    take the job out of the loop

--- app/jobs/download_job.rb
+++ app/jobs/download_job.rb
@@ -1,19 +1,21 @@
 class DownloadJob
   include Sidekiq::Worker
-  sidekiq_options queue:'download_file', retry: 10
+  sidekiq_options queue:'download_file', retry: 3#10
 
   sidekiq_retry_in do |count|
-     600
+     10#600
   end
 
   sidekiq_retries_exhausted do |msg, e|
     Rails.logger.warn "Failed #{msg['class']} with #{msg['args']}: #{msg['error_message']}"
   end
 
-  def perform(file_name, client_name)
-    file_path = "#{Rails.root}/public/#{client_name}/#{file_name}"
-    Net::SFTP.start("test.rebex.net", "demo", password: ("password")) do |sftp|
-      sftp.download! "/pub/example/#{file_name}", file_path
+  def perform(file_names, client_name)
+    file_names.each do |file_name|
+      file_path = "#{Rails.root}/public/#{client_name}/#{file_name}"
+      Net::SFTP.start("test.rebex.net", "demo", password: ("password")) do |sftp|
+        sftp.download! "/pub/example/#{file_name}", file_path
+      end
     end
   end
 end
--- lib/sftp_download.rb
+++ lib/sftp_download.rb
@@ -2,10 +2,8 @@ class SftpDownload
   def self.process_file_for_download(client_id)
     client = Client.find(client_id)
     client_name = client.name
+    client_files = client.client_files.map(&:file_format)
     Dir.mkdir "#{Rails.root}/public/#{client_name}/"
-    client.client_files.each do |file|
-      file_name = file.file_format
-      DownloadJob.perform_async(file_name, client_name)
-    end
+    DownloadJob.perform_async(client_files, client_name)
   end
 end
