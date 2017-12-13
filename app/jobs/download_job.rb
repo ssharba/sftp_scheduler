c34a2a02e31bb2c18fc9ba27c71de6e3586f973b
louy755 <lumberjack1694@yahoo.com>
2017-12-13 09:23:15 -0600

    modify1

--- app/jobs/download_job.rb
+++ app/jobs/download_job.rb
@@ -10,12 +10,20 @@ class DownloadJob
     Rails.logger.warn "Failed #{msg['class']} with #{msg['args']}: #{msg['error_message']}"
   end
 
-  def perform(file_names, client_name)
+  def perform(client_id)
+    client = Client.find(client_id)
+    file_names = client.client_files.map(&:file_format)
     file_names.each do |file_name|
-      file_path = "#{Rails.root}/public/#{client_name}/#{file_name}"
-      Net::SFTP.start("test.rebex.net", "demo", password: ("password")) do |sftp|
-        sftp.download! "/pub/example/#{file_name}", file_path
-      end
+      download_file(file_name, client.name)
+    end
+  end
+
+  private
+
+  def download_file(file_name, client_name)
+    file_path = "#{Rails.root}/public/#{client_name}/#{file_name}"
+    Net::SFTP.start("test.rebex.net", "demo", password: ("password")) do |sftp|
+      sftp.download! "/pub/example/#{file_name}", file_path
     end
   end
 end
--- lib/sftp_download.rb
+++ lib/sftp_download.rb
@@ -2,8 +2,7 @@ class SftpDownload
   def self.process_file_for_download(client_id)
     client = Client.find(client_id)
     client_name = client.name
-    client_files = client.client_files.map(&:file_format)
     Dir.mkdir "#{Rails.root}/public/#{client_name}/"
-    DownloadJob.perform_async(client_files, client_name)
+    DownloadJob.perform_async(client.id)
   end
 end
