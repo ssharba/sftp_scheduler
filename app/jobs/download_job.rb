e1122bfbc15741a918c83374ca6af373bb2c249a
louy755 <lumberjack1694@yahoo.com>
2017-12-13 14:17:20 -0600

    modify2

--- app/jobs/download_job.rb
+++ app/jobs/download_job.rb
@@ -1,9 +1,9 @@
 class DownloadJob
   include Sidekiq::Worker
-  sidekiq_options queue:'download_file', retry: 10
+  sidekiq_options queue:'download_file', retry: 3#10
 
   sidekiq_retry_in do |count|
-     600
+     10#600
   end
 
   sidekiq_retries_exhausted do |msg, e|
@@ -14,16 +14,8 @@ class DownloadJob
     client = Client.find(client_id)
     file_names = client.client_files.map(&:file_format)
     file_names.each do |file_name|
-      download_file(file_name, client.name)
-    end
-  end
-
-  private
-
-  def download_file(file_name, client_name)
-    file_path = "#{Rails.root}/public/#{client_name}/#{file_name}"
-    Net::SFTP.start("test.rebex.net", "demo", password: ("password")) do |sftp|
-      sftp.download! "/pub/example/#{file_name}", file_path
+      file_properties = { file_name: file_name, client_name: client.name }
+      SftpDownload.new(file_properties).call
     end
   end
 end
