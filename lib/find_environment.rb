ab9b852e7555a525fa8e81f8c338a68e56f38f2d
louy755 <lumberjack1694@yahoo.com>
2017-12-15 10:48:20 -0600

    lookup changes

--- lib/find_environment.rb
+++ lib/find_environment.rb
@@ -1,7 +1,14 @@
 class FindEnvironment
   class << self
     def environment_for(client_id)
-      $ENVIRONMENTS["client#{client_id}"]
+      environment = $ENVIRONMENTS.detect do |client_environment|
+        clients = client_environment[:clients]
+        clients.detect do |client|
+          client[:client_id] == client_id
+        end
+      end
+      return false unless environment
+      environment[:env_id]
     end
   end
 end
