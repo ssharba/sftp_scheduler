aa9c1a9a1392c7232de015f7d02f269a3c2a6446
mwmaki <mwmaki2@gmail.com>
2017-12-26 11:26:49 -0600

    add the retry count and build logic on it

---
+++ app/jobs/test_retry_worker.rb
@@ -0,0 +1,22 @@
+class TestRetryWorker
+  include Sidekiq::Worker
+
+  # create unattribut called retry_count for sidekiq
+  attr_accessor :retry_count
+
+  # This will increase the count by 1, Note it's initial value is 0
+  def retry_count
+    @retry_count.nil? ? 0 : @retry_count+1
+  end
+
+  # To make the retry happend faster  for testing purposes
+  sidekiq_retry_in do |count|
+    1
+  end
+
+
+  def perform(*args)
+    puts "Job ID => #{self.jid} == Count times ==> #{retry_count} = will stop in count less than 3 " if retry_count <3
+    raise 'This is TEST ERROR'
+  end
+end
