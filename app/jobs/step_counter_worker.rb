9f063ac79c42b8d1d8cdf5d24a092631aac1e241
mwmaki <mwmaki2@gmail.com>
2017-12-27 20:19:38 -0600

    step counter implementation

---
+++ app/jobs/step_counter_worker.rb
@@ -0,0 +1,37 @@
+class StepCounterWorker
+  include Sidekiq::Worker
+
+  def step_counter
+    @step.nil? ? @step = 1 : @step += 1
+  end
+
+  def perform(*args)
+    puts "++++++++#{step_counter}++++++++"
+    puts "++++++++#{step_counter}++++++++"
+    puts "++++++++#{step_counter}++++++++"
+    puts "++++++++#{step_counter}++++++++"
+    puts "++++++++#{step_counter}++++++++"
+    puts "++++++++#{step_counter}++++++++"
+    puts ""
+    raise 'This is TEST ERROR'
+  end
+
+
+=begin  THIS IS THE RESULT IN SIDEKIQ TERMINAL
+++++++++1++++++++
+++++++++2++++++++
+++++++++3++++++++
+++++++++4++++++++
+++++++++5++++++++
+++++++++6++++++++
+
+++++++++1++++++++
+++++++++2++++++++
+++++++++3++++++++
+++++++++4++++++++
+++++++++5++++++++
+++++++++6++++++++
+=end
+
+
+end
