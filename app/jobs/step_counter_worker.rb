5f44394f906abcaf8bd2d5adfa6837bb5f653cd0
mwmaki <mwmaki2@gmail.com>
2017-12-27 23:59:51 -0600

    refactor the step_counter code

--- app/jobs/step_counter_worker.rb
+++ app/jobs/step_counter_worker.rb
@@ -1,37 +1,37 @@
 class StepCounterWorker
   include Sidekiq::Worker
 
-  def step_counter
-    @step.nil? ? @step = 1 : @step += 1
+  # To make the retry happend faster  for testing purposes
+  sidekiq_retry_in do |count|
+    1
   end
 
   def perform(*args)
-    puts "++++++++#{step_counter}++++++++"
-    puts "++++++++#{step_counter}++++++++"
-    puts "++++++++#{step_counter}++++++++"
-    puts "++++++++#{step_counter}++++++++"
-    puts "++++++++#{step_counter}++++++++"
-    puts "++++++++#{step_counter}++++++++"
-    puts ""
-    raise 'This is TEST ERROR'
+
+    if HelperMethods.step_counter == 1
+      puts "1 +++++"
+      raise "error number 1"
+    end
+    #HelperMethods.step_counter =3
+    if HelperMethods.step_counter == 3
+      puts "3 +++++"
+      raise "error number 3"
+    end
+
+    if HelperMethods.step_counter == 6
+      puts "6 +++++"
+      raise "error number 6"
+    end
+
+    if HelperMethods.step_counter == 10
+      puts "10 +++++"
+      raise "error number 10"
+    end
   end
-
-
 =begin  THIS IS THE RESULT IN SIDEKIQ TERMINAL
-++++++++1++++++++
-++++++++2++++++++
-++++++++3++++++++
-++++++++4++++++++
-++++++++5++++++++
-++++++++6++++++++
-
-++++++++1++++++++
-++++++++2++++++++
-++++++++3++++++++
-++++++++4++++++++
-++++++++5++++++++
-++++++++6++++++++
+1 +++++
+3 +++++
+6 +++++
+10 +++++
 =end
-
-
 end
